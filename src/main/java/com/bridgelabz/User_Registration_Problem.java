65b2f6ad5c6a7e03ed58085f9ecf7989b0d30f44
SSK137 <saurabhkodam@gmail.com>
2022-05-09 23:49:37 +0530

    Java Rejex: For First Name

--- src/main/java/com/bridgelabz/User_Registration_Problem.java
+++ src/main/java/com/bridgelabz/User_Registration_Problem.java
@@ -7,15 +7,24 @@ public class User_Registration_Problem {
     public static boolean First_Name(){
         System.out.println("Enter the First Name : ");
         String F_Name=scanner.next();
-        String rejex="^[A-Z]{1}[a-z]{2,}$";
-        Pattern P=Pattern.compile(rejex);
+        System.out.println("Enter the Last Name : ");
+        String L_Name=scanner.next();
+        String Fname_rejex="^[A-Z]{1}[a-z]{2,}$";
+        String Lname_rejex="^[A-Z]{1}[a-z]{2,}$";
+        Pattern P=Pattern.compile(Fname_rejex);
+        Pattern P1=Pattern.compile(Lname_rejex);
         Matcher M=P.matcher(F_Name);
+        Matcher M1=P.matcher(L_Name);
         boolean R=M.matches();
+        boolean R1=M1.matches();
 
         if(R)
-            System.out.println("Entered Name is Valid");
+            if (R1)
+                System.out.println("Entered First Name and Last Name is Valid");
+            else
+                System.out.println("You Entered Invalid Last Name");
         else
-            System.out.println("Entered Name is Invalid");
+            System.out.println("Entered Invalid First Name ");
         return R;
     }
 }
