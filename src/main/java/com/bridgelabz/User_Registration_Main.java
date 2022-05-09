dea21f8492198aee122af75407a9a4bf3c8c345d
SSK137 <saurabhkodam@gmail.com>
2022-05-09 21:30:37 +0530

    Java Rejex: For First Name

---
+++ src/main/java/com/bridgelabz/User_Registration_Main.java
@@ -0,0 +1,9 @@
+package com.bridgelabz;
+
+public class User_Registration_Main {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Java Rejex Problems");
+        User_Registration_Problem userRegistrationProblem=new User_Registration_Problem();
+        userRegistrationProblem.First_Name();
+    }
+}
--- src/main/java/com/bridgelabz/User_Registration_Problem.java
+++ src/main/java/com/bridgelabz/User_Registration_Problem.java
@@ -2,7 +2,20 @@ package com.bridgelabz;
 import java.util.*;
 import java.util.regex.*;
 public class User_Registration_Problem {
-    public static void main(String[] args) {
-        System.out.println("Welcome to the Java Rejex Problems");
+    public static Scanner scanner=new Scanner(System.in);
+
+    public static boolean First_Name(){
+        System.out.println("Enter the First Name : ");
+        String F_Name=scanner.next();
+        String rejex="^[A-Z]{1}[a-z]{2,}$";
+        Pattern P=Pattern.compile(rejex);
+        Matcher M=P.matcher(F_Name);
+        boolean R=M.matches();
+
+        if(R)
+            System.out.println("Entered Name is Valid");
+        else
+            System.out.println("Entered Name is Invalid");
+        return R;
     }
 }
