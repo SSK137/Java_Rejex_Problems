59c5ff42087a8658670adf25f5ee3257057f2642
SSK137 <saurabhkodam@gmail.com>
2022-05-09 19:30:43 +0530

    Java Rejex: Welcome Message

---
+++ src/main/java/com/bridgelabz/User_Registration_Problem.java
@@ -0,0 +1,8 @@
+package com.bridgelabz;
+import java.util.*;
+import java.util.regex.*;
+public class User_Registration_Problem {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Java Rejex Problems");
+    }
+}
